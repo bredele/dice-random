4de31baaa1b67c41055be03a0fdee890ba76406d
bredele <olivier.wietrich@gmail.com>
2018-12-14 09:50:02 -0700

    Generate cryptographically strong random number

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Check if getRandomValues exist in browser.
+ * @type {Function}
+ */
+
+if (typeof window !== 'undefined') {
+  var random = window.crypto && window.crypto.getRandomValues.bind(crypto)
+}
+
+/**
+ * Generate cryptographically strong random number.
+ *
+ * @param {Number?} range
+ * @api public
+ */
+
+module.exports = (range = 5) => {
+  var nb
+  if (random) {
+    const array = new Uint32Array(10)
+    random(array)
+    nb = array[0]
+  } else {
+    nb = (Math.random() * 0x100000000) | 0
+  }
+  return Math.abs((nb % range) + 1)
+}
---
+++ test/dice-random.js
@@ -0,0 +1,28 @@
+/**
+ * Dependencies
+ */
+
+const test = require('tape')
+const random = require('..')
+
+
+test('should generate a number between 1 and 6', assert => {
+  let plan = 100
+  assert.plan(plan * 2)
+  while (plan--) {
+    const nb = random()
+    assert.equal(nb > -1, true)
+    assert.equal(nb < 7, true)
+  }
+})
+
+test('should generate a number between 1 and a given range', assert => {
+  let plan = 100
+  const range = 12
+  assert.plan(plan * 2)
+  while (plan--) {
+    const nb = random(range)
+    assert.equal(nb > -1, true)
+    assert.equal(nb < (range + 2), true)
+  }
+})
